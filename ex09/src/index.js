f49e0b4c078c5f5f78049f24caf9f4f12bb1667a
dino-ha <hadzirovicdino@gmail.com>
2022-03-30 20:13:49 +0200

    Add files via upload

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,1 +1,13 @@
+function main(numOne, numTwo) {
+	// Only change code below this line
+	
+	var difference, numOne, numTwo;
+	difference = numOne - numTwo;
+	// Only change code above this line
+	return difference;
 
+}
+
+console.log(main(numOne, numTwo));
+
+module.exports = main;
