58c69919b50d75082b60137cb9984db7301e18d3
dino-ha <hadzirovicdino@gmail.com>
2022-03-30 20:02:35 +0200

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,18 @@
+function main() {
+	// Only change code below this line
+	
+	//this is a single line comment
+	
+	/* this 
+	is a 
+	multiline
+	comment
+	*/
 
+	// Only change code above this line
+;
+}
+
+console.log(main());
+
+module.exports = main;
